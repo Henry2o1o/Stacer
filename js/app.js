03495655a87b5e78e1bfb9fc75864b9fa0c2e0a4
QuentiumYT <quentin.lienhardt@gmail.com>
2024-12-17 22:04:42 +0100

    Update website

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const youtube = document.getElementById('youtube');
+
+youtube.addEventListener('click', function () {
+    const iframe_url = "https://www.youtube.com/embed/gOUbCjUH47s?autoplay=1&autohide=1&rel=0&start=4&modestbranding=1&showinfo=0&controls=0&hd=hd720";
+
+    const iframe = `<iframe frameborder="0" class="cast-shadow" src="${iframe_url}" style="width: ${youtube.offsetWidth}px; height: ${youtube.offsetHeight}px;"></iframe>`;
+
+    youtube.innerHTML = iframe;
+});
